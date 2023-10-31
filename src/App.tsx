c9a830a37050d4dd79d354983005c9d7b29dd56c
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2023-10-31 23:29:34 +0300

    set up environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const years: number = 26;
 
 function App() {
   return (
